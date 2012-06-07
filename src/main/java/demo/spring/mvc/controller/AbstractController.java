004aa989ca69f33b266aa8351507df1236ef633f
Akuma Huang <ihuangwj@gmail.com>
2012-06-07 14:34:34 +0800

    添加异常拦截方法并完善 javadoc

--- src/main/java/demo/spring/mvc/controller/AbstractController.java
+++ src/main/java/demo/spring/mvc/controller/AbstractController.java
@@ -5,6 +5,8 @@
  */
 package demo.spring.mvc.controller;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -22,10 +24,18 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
- * Controller 抽象类，提供了添加错误、提示等消息的方法。
+ * <code>Controller</code> 抽象类，可供业务 <code>Controller</code> 继承。主要提供了以下功能：
+ * 
+ * <ul>
+ * <li>提供了添加错误信息、提示信息的相关方法。</li>
+ * <li>对异常进行统一拦截，如果是 AJAX 请求会 dispatch 到 JSON 异常页面，否则 dispatch 到普通异常页面。</li>
+ * </ul>
  * 
  * @author wj.huang
  * @version $Revision: 5 $, $Date: 2012-04-06 13:24:48 +0800 (Fri, 06 Apr 2012) $
@@ -37,6 +47,12 @@ public abstract class AbstractController implements ValidationSupport {
     private static final String ACTION_ERRORS = "actionErrors";
     private static final String ACTION_MESSAGES = "actionMessages";
     private static final String FIELD_ERRORS = "fieldErrors";
+
+    private static final String AJAX_REQUEST_HEADER_NAME = "X-Requested-With";
+    private static final String AJAX_REQUEST_HEADER_VALUE = "XMLHttpRequest";
+
+    private String exceptionViewName = "exceptionPage";
+    private String exceptionJsonViewName = "exceptionJson";
 
     @Resource
     protected MessageSource messageSource;
@@ -207,6 +223,88 @@ public abstract class AbstractController implements ValidationSupport {
         message.setFieldErrors(fieldErrorsMap);
 
         return message;
+    }
+
+    /**
+     * 处理异常 {@link Exception} 的方法，它会记录系统日志然后跳转到异常页面。
+     * 
+     * @param ex
+     *            异常信息
+     * @param request
+     *            Spring WebRequest
+     * @return 异常视图
+     */
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception ex, WebRequest request) {
+        logger.error(ex.getMessage(), ex);
+
+        StringWriter stackTrace = new StringWriter();
+        ex.printStackTrace(new PrintWriter(stackTrace));
+
+        ExceptionResponse response = new ExceptionResponse();
+        response.setMessage(ex.getMessage());
+        response.setStackTrace(stackTrace.toString());
+        request.setAttribute("exception", response, RequestAttributes.SCOPE_REQUEST);
+
+        return isAjaxRequest(request) ? exceptionJsonViewName : exceptionViewName;
+    }
+
+    /**
+     * 设置异常拦截时需要跳转的视图名称，默认为 exceptionPage。
+     * 
+     * @param exceptionViewName
+     *            view name
+     */
+    public void setExceptionViewName(String exceptionViewName) {
+        this.exceptionViewName = exceptionViewName;
+    }
+
+    /**
+     * 设置异常拦截时需要跳转的 JSON 视图名称，默认为 exceptionJson。
+     * 
+     * @param exceptionJsonViewName
+     *            view name
+     */
+    public void setExceptionJsonViewName(String exceptionJsonViewName) {
+        this.exceptionJsonViewName = exceptionJsonViewName;
+    }
+
+    /**
+     * 判断请求是否是 Ajax 请求。
+     * 
+     * @param request
+     *            Spring WebRequest
+     * @return true/false
+     */
+    protected boolean isAjaxRequest(WebRequest request) {
+        String header = request.getHeader(AJAX_REQUEST_HEADER_NAME);
+        return AJAX_REQUEST_HEADER_VALUE.equalsIgnoreCase(header);
+    }
+
+    /**
+     * 异常消息响应封装类。
+     */
+    public static class ExceptionResponse {
+
+        private String message;
+        private String stackTrace;
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public String getStackTrace() {
+            return stackTrace;
+        }
+
+        public void setStackTrace(String stackTrace) {
+            this.stackTrace = stackTrace;
+        }
+
     }
 
 }
