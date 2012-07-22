9b9ab1e16abe2fba7c4c755dfa69c1218b3a7436
Akuma Huang <ihuangwj@gmail.com>
2012-07-22 10:19:14 +0800

    添加测试 json 输出编码的方法

--- src/main/java/demo/spring/mvc/controller/HelloWorldController.java
+++ src/main/java/demo/spring/mvc/controller/HelloWorldController.java
@@ -4,10 +4,18 @@
  */
 package demo.spring.mvc.controller;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.guomi.meazza.spring.mvc.JsonViewHelper;
 
 /**
  * Hello World 控制器。
@@ -26,6 +34,19 @@ public class HelloWorldController {
     @ResponseBody
     public String hello() {
         return "Hello, Spring MVC!";
+    }
+
+    @RequestMapping("/jsonTest")
+    public void jsonTest(HttpServletResponse response) throws IOException {
+        String json = "{\"test\":\"哈哈哈，看看是否是中文。\"}";
+
+        // response.setContentType("application/json");
+        // response.getWriter().print(json);
+        // response.getWriter().flush();
+
+        Map<String, String> model = new HashMap<>();
+        model.put("test", json);
+        JsonViewHelper.render(model, response);
     }
 
 }
