5b8f2705e02423118070b76e00978fe281f71ee7
Akuma Huang <ihuangwj@gmail.com>
2013-01-12 10:20:04 +0800

    整理测试代码

--- src/test/java/demo/spring/mvc/dao/impl/UserDaoImplTest.java
+++ src/test/java/demo/spring/mvc/dao/impl/UserDaoImplTest.java
@@ -4,18 +4,20 @@
  */
 package demo.spring.mvc.dao.impl;
 
-import java.sql.SQLException;
-import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.io.Serializable;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.guomi.meazza.test.BasicTestCase;
-import com.guomi.meazza.util.Pagination;
 
 import demo.spring.mvc.dao.UserDao;
 import demo.spring.mvc.entity.User;
@@ -25,87 +27,71 @@ import demo.spring.mvc.entity.User;
  */
 public class UserDaoImplTest extends BasicTestCase {
 
-    private static final Logger logger = LoggerFactory.getLogger(UserDaoImplTest.class);
-
     @Resource
     private UserDao userDao;
 
-    /**
-     * test.
-     */
     @Test
-    public void testFindSerializable() {
-        try {
-            throw new Exception("Test exception occurred", new SQLException("SQL test exception occurred",
-                    "This is just a SQL test exception."));
-        } catch (Exception ex) {
-            // logger.error(ex);
-            logger.error("An exception occurred", ex);
-        }
-    }
-
-    /**
-     * test.
-     */
-    @Test
-    public void testFindUser() {
-    }
-
-    /**
-     * test.
-     */
-    @Test
-    public void testFindUserPagination() {
-        User user = new User();
-        user.setUsername("huangwj");
-        user.setRealName("黄维佳");
-
-        Pagination page = new Pagination();
-        page.setPageNum(2);
-
-        List<User> userList = userDao.find(user, page);
-        System.out.println(userList);
-    }
-
-    /**
-     * test.
-     */
-    @Test
-    public void testFindSerializableArray() {
-    }
-
-    /**
-     * test.
-     */
-    @Test
-    public void testInsertUserArray() {
+    public void testCrudOps() {
         User user = new User();
         user.setUsername(RandomStringUtils.randomAlphabetic(12));
         user.setPassword("123456");
         user.setRealName("测试用户");
 
         userDao.insert(user);
+
+        User dbUser = userDao.find(user.getId());
+        assertNotNull(dbUser);
+        assertEquals(dbUser.getId(), user.getId());
+
+        user.setUsername(RandomStringUtils.randomAlphabetic(12));
+        user.setPassword("123abc");
+        user.setRealName("测试用户2");
+        userDao.update(user);
+
+        dbUser = userDao.find(user.getId());
+        assertNotNull(dbUser);
+        assertEquals(dbUser.getId(), user.getId());
+        assertEquals(dbUser.getUsername(), user.getUsername());
+        assertEquals(dbUser.getPassword(), user.getPassword());
+        assertEquals(dbUser.getRealName(), user.getRealName());
+
+        // Update if necessary
+        user.setUsername(null);
+        user.setPassword(null);
+        user.setRealName(null);
+        userDao.updateNotNull(user);
+
+        dbUser = userDao.find(user.getId());
+        assertNotNull(dbUser);
+        assertEquals(dbUser.getId(), user.getId());
+        assertFalse(dbUser.getUsername().equals(user.getUsername()));
+        assertFalse(dbUser.getPassword().equals(user.getPassword()));
+        assertFalse(dbUser.getRealName().equals(user.getRealName()));
+
+        userDao.delete(user.getId());
+        dbUser = userDao.find(user.getId());
+        assertNull(dbUser);
     }
 
-    /**
-     * test.
-     */
     @Test
-    public void testUpdateUserArray() {
-    }
+    public void testBatchOps() {
+        // Batch insert
+        User user1 = new User();
+        user1.setUsername(RandomStringUtils.randomAlphabetic(12));
+        user1.setPassword("123456");
+        user1.setRealName("测试用户");
 
-    /**
-     * test.
-     */
-    @Test
-    public void testUpdateNotNull() {
-    }
+        User user2 = new User();
+        user2.setUsername(RandomStringUtils.randomAlphabetic(12));
+        user2.setPassword("123456");
+        user2.setRealName("测试用户");
 
-    /**
-     * test.
-     */
-    @Test
-    public void testDeleteSerializableArray() {
+        // userDao.inserts(user1, user2);
+        userDao.insert(user1);
+        userDao.insert(user2);
+
+        Map<Serializable, User> users = userDao.find(user1.getId(), user2.getId());
+        System.out.println(users);
     }
 
 }
